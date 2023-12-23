81f4e4dbbc631893878449f9898dfcbb1ae6754e
ChernyshovVadym <vad14@i.ua>
2023-12-23 13:02:12 +0200

    add

--- js/task-2.js
+++ js/task-2.js
@@ -9,7 +9,27 @@
 // addItem(newItem) — приймає новий товар newItem і додає його до масиву товарів у приватну властивість items об'єкта.
 // removeItem(itemToRemove) — приймає рядок з назвою товару itemToRemove і видаляє його з масиву товарів у приватній властивості items об'єкта.
 
+class Storage {
+    #items;
+    constructor(items) {
+        this.#items = items;
+    }
 
+    getItems() {
+        return this.#items;
+    }
+
+    addItem(newItem) {
+        this.#items.push(newItem);
+    }
+
+    removeItem(itemToRemove) {
+        const index = this.#items.indexOf(itemToRemove);
+        if (index !== -1) {
+            this.#items.splice(index, 1);
+        }
+    }
+}
 
 
 
